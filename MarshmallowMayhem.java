e3c98dd50c5bb026db5de9448c864e102d1e60e1
Dhannani <Pokerapboy@yahoo.com>
2017-01-22 22:06:41 -0800

    Add files via upload

---
+++ MarshmallowMayhem.java
@@ -0,0 +1,34 @@
+package application;
+
+import java.io.*;
+import java.util.*;
+
+//import com.sun.corba.se.spi.orbutil.fsm.Input;
+
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
+public class MarshmallowMayhem extends Application 
+{
+	@Override
+	public void start(Stage primaryStage) 
+	{
+		primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() 
+		{
+			public void handle(WindowEvent event) 
+			{
+				System.exit(0);
+			}
+		});
+		new Menu(primaryStage).displayMenu();
+		primaryStage.setTitle("Marshmallow Mayhem");
+		primaryStage.show();
+	}
+
+	public static void main(String[] args) 
+	{		
+		launch(args);
+	}
+}
